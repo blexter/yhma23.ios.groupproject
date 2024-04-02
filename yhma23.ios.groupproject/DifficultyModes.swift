76facad9b4d9347a091842f154339626f31a2a31
Linus Ilbratt <linusilbratt@gmail.com>
2024-04-02 09:19:53 +0200

    moved logic into GameModel from controller to make the structure more MVC

--- yhma23.ios.groupproject/DifficultyModes.swift
+++ yhma23.ios.groupproject/DifficultyModes.swift
@@ -11,59 +11,29 @@ import UIKit
 
 class DifficultyModes: UIViewController {
     
-    let words: [String] = ["easy1", "easy2", "easy3", "easy4", "easy5", "medium1", "medium2", "medium3", "medium4", "medium5", "hard1", "hard2", "hard3", "hard4", "hard5"]
-    var selectedWords: [String] = []
-
+    var gameModel = GameModel()
+    
     @IBAction func easyMode(_ sender: UIButton) {
-        startGame(withWords: Array(words[0...4]))
+        startGame(withDifficulty: .easy)
     }
     
     @IBAction func mediumMode(_ sender: Any) {
-        startGame(withWords: Array(words[5...9]))
+        startGame(withDifficulty: .medium)
     }
     
     @IBAction func hardMode(_ sender: Any) {
-        startGame(withWords: Array(words[10...14]))
+        startGame(withDifficulty: .hard)
     }
     
-    func startGame(withWords words: [String]) {
-        // Store the selected words in a property
-        selectedWords = words
-
-        // Perform the segue
+    func startGame(withDifficulty difficulty: Difficulty) {
+        gameModel.selectWords(for: difficulty)
         performSegue(withIdentifier: "startGame", sender: self)
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "startGame" {
-            if let gameViewController = segue.destination as? GameViewController {
-                gameViewController.words = selectedWords
-            }
+        if segue.identifier == "startGame", let gameViewController = segue.destination as? GameViewController {
+            gameViewController.words = gameModel.selectedWords
         }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
-
-
-    
-
-    
-    
-
-
-
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
 
--- yhma23.ios.groupproject/GameModel.swift
+++ yhma23.ios.groupproject/GameModel.swift
@@ -9,15 +9,38 @@ import Foundation
 
 class GameModel {
     
+    private let words: [String] = ["easy1", "easy2", "easy3", "easy4", "easy5", "medium1", "medium2", "medium3", "medium4", "medium5", "hard1", "hard2", "hard3", "hard4", "hard5"]
+    
+    var selectedWords: [String] = []
+    
+    func selectWords(for difficulty: Difficulty) {
+        // logic for choosing words based on difficulty
+        switch difficulty {
+        case .easy:
+            selectedWords = Array(words[0...4])
+        case .medium:
+            selectedWords = Array(words[5...9])
+        case .hard:
+            selectedWords = Array(words[10...14])
+            break
+        }
+    }
+    
 }
 
-struct word {
+struct Word {
     var text : String
     var isCompleted : Bool = false
 }
 
-struct points {
+struct Points {
     var correctWords : Int = 0
     var incorrectWords : Int = 0
     var playerName : String
+}
+
+enum Difficulty {
+    case easy
+    case medium
+    case hard
 }
