9a252f5e0e56109c9f86b90a288d88f3d81f3102
Linus Ilbratt <linusilbratt@gmail.com>
2024-04-02 20:18:54 +0200

    added a storyboard view connected to the tab bar controller for the player view

---
+++ yhma23.ios.groupproject/PlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerViewController.swift
+//  yhma23.ios.groupproject
+//
+//  Created by Linus Ilbratt on 2024-04-02.
+//
+
+import UIKit
+
+class PlayerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
